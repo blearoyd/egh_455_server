6f5ac547b6f2fb8af5b1ad3d6b17b29dee82ea33
blearoyd <brendanlearoyd1@gmail.com>
2021-09-13 16:26:36 +1000

    selects last 10 mins be default

--- routes/enviroment.js
+++ routes/enviroment.js
@@ -6,13 +6,18 @@ let db = require('knex')(config)
 
 router.get('/:column', function(req, res, next) {
 
-    db.select('time', req.params.column).table('Enviro')
+    // Runs this query: SELECT * FROM Enviro WHERE TIME>DATE_ADD((SELECT MAX(TIME) FROM Enviro),INTERVAL-10 MINUTE) 
+    // To retrive last 10 mins of data
+    db.select('time', req.params.column)
+    .table('Enviro')
+    .where('time', '>',db.raw('DATE_ADD(?, INTERVAL ? MINUTE)',[db('Enviro').max('time'), -10]))
     .then( rows => {
         res.send(rows)
     })
     .catch(err => {
         console.log(err)
     })
+
     
     
 });
